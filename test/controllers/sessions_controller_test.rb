42c45f76c9b928ebfcd46dc0abae2745bf1300f4
Derek Loe <dealsloe@yahoo.com>
2015-04-28 21:13:52 -0500

    Finshed user, edit , update, dinex, and destroy actions. However, when I run the rake test, failure of UsersIndexTest#test_index_as_admin_including_pagination_and_delete_links with Expected at least 1 element matching div.pagination, found 0.. Expected 0 to be >= 1. I cannot figure out a way to debug it as of now.

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,9 +1,13 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
+
+	def setup
+    @user = users(:derek)
+  end
+
   test "should get new" do
     get :new
     assert_response :success
   end
-
 end
